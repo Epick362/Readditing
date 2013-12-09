6570a1cfa9a8d16a4d4709848c0bfe9f2b02d347
Epick362 <flp.hajek@gmail.com>
2013-12-09 14:59:36 -0600

    comments

--- application/views/r.php
+++ application/views/r.php
@@ -6,4 +6,4 @@
 ?>
 </div>
 <div id="loading" class="alert alert-info" style="display:none"><i class="icon-refresh icon-spin"></i> Loading...</div>
-<a id="next-page" class="btn btn-block btn-primary" style="margin-bottom:40px" href="<?=base_url('r/'.$subreddit.'/'.$show.'/'.$post->data->id)?>">Next Page</a>
+<!-- <a id="next-page" class="btn btn-block btn-primary" style="margin-bottom:40px" href="<?=base_url('r/'.$subreddit.'/'.$show.'/'.$post->data->id)?>">Next Page</a> -->
