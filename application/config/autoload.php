0e363f3d941ed6fd20a543ea88b99a5628f478f5
Epick362 <flp.hajek@gmail.com>
2013-12-18 15:45:08 -0600

    Revert 5ae55f0..6be677d
    This rolls back to commit 5ae55f047e01b03920572ed1fca59583d19779bc.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('REST', 'display', 'template', 'session', 'reddit', 'OAuth2');
+$autoload['libraries'] = array('REST', 'display', 'template', 'session', 'reddit');
 
 
 /*
