8c505f63ed3720a9b2074712c7df34ab008fc77a
Epick362 <flp.hajek@gmail.com>
2013-12-20 15:41:51 -0600

    ...

--- application/libraries/reddit.php
+++ application/libraries/reddit.php
@@ -128,7 +128,7 @@ class reddit{
 
                 $item->_display = $this->_ci->display->{$item->kind}($item);
             }else{
-                unset($item); // TEMPORARY
+                echo '<pre>'; print_r($item); echo '</pre>';
             }
         }
 
