deaac247fcbc5d7bf8480b1e6b474ebecdfbb2f1
Epick362 <flp.hajek@gmail.com>
2013-12-20 15:50:14 -0600

    Comments

--- application/views/user.php
+++ application/views/user.php
@@ -3,7 +3,15 @@
 <div id="feed">
 <?
 	foreach($feed as $key => $post) {
-		echo $this->load->view('post_template', array('post' => $post, 'user' => $user));
+		if($post->kind == 'comment') {
+			echo '<div class="panel panel-reddit">';
+			echo '<div class="panel-body">';
+			echo $this->load->view('comment_template', array('comment' => $post));
+			echo '</div>';
+			echo '</div>';
+		}else{
+			echo $this->load->view('post_template', array('post' => $post, 'user' => $user));
+		}
 	}
 ?>
 </div>
