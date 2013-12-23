0b67e8d942b8f20df5574a3af4e759c1573c259f
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-23 12:09:36 -0600

    constructor

--- application/libraries/storage.php
+++ application/libraries/storage.php
@@ -1,5 +1,10 @@
 <?php
 	class storage{
+		function __construct() {
+			$this->_ci =& get_instance();
+			log_message('debug', 'Storage Library Initialized');
+		}
+
 		public function articleDatabase($url) {
 			print_r($this->mongo_db->where(array('url' => $url))->get('articles'));
 		}
