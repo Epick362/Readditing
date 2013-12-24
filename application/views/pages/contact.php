2803823e1696b1814aee20f44dccfbd3a224b5cd
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-23 20:03:45 -0600

    Contact

---
+++ application/views/pages/contact.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div class="modal fade" id="contact" tabindex="-1" role="dialog" aria-labelledby="contactLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="contactLabel">Contact</h4>
+      </div>
+      <div class="modal-body">
+		<address>
+		  <strong>Readditing</strong><br>
+		  <strong>Filip Hajek</strong><br>
+		  <a>flp.hajek[at]gmail.com</a>
+		</address>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
