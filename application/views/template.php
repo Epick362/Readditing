e40aa7d08800bcff5b36ca28e92ab2d3e02399c9
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-27 12:11:14 -0600

    Typo Fix

--- application/views/template.php
+++ application/views/template.php
@@ -83,7 +83,7 @@
 					<div class="sidebar panel panel-reddit">
 						<div class="panel-heading">Settings</div>
 						<div class="panel-body">
-							<?=form_open(base_url('user/nsfw'), array('class' => 'navbar-form'), array('back' => uri_string(current_url()))))?>
+							<?=form_open(base_url('user/nsfw'), array('class' => 'navbar-form'), array('back' => uri_string(current_url())))?>
 								<input type="checkbox" name="nsfw" id="nsfw-checkbox" data-on="success" data-off="danger" <? if($this->session->userdata('nsfw')) { echo 'checked'; }?>>
 							<?=form_close()?>
 						</div>
