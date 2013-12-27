a49b595bd17e8820d45a884c7e930f136af123ae
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-27 12:49:10 -0600

    Small Fixes

--- application/views/settings_template.php
+++ application/views/settings_template.php
@@ -3,8 +3,8 @@
 		<div class="panel-heading">Settings</div>
 		<div class="panel-body">
 			<?=form_open(base_url('user/nsfw'), array('class' => 'form-inline'), array('back' => uri_string(current_url())))?>
-				<?=form_label('Show NSFW', 'nsfw')?>
-				<input type="checkbox" name="nsfw" id="nsfw-checkbox" data-on="success" data-off="danger" <? if($this->session->userdata('nsfw')) { echo 'checked'; }?>>
+				<?=form_label('Show NSFW', 'nsfw')?> 
+				<input type="checkbox" class="switch-small" name="nsfw" id="nsfw-checkbox" data-on="success" data-off="danger" <? if($this->session->userdata('nsfw')) { echo 'checked'; }?>>
 			<?=form_close()?>
 		</div>
 	</div>
