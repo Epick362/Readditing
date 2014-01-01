3c4a4524a02fde45cab76b885665e8594f713436
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2013-12-31 19:06:18 -0600

    Small Fix 3

--- application/views/r.php
+++ application/views/r.php
@@ -1,7 +1,7 @@
 <div id="feed">
 <?
 	foreach($feed as $key => $post) {
-		echo $this->load->view('post_template', array('post' => $post, 'user' => $user));
+		echo $this->load->view('templates/post_template', array('post' => $post, 'user' => $user));
 	}
 ?>
 </div>
