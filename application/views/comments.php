f56d8ce8e3153048ec9efa6ac9b1154651a1d3cb
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-01-01 14:51:07 -0600

    .

--- application/views/comments.php
+++ application/views/comments.php
@@ -1,3 +1,3 @@
 <div id="post">
-	<?=$this->load->view('templates/post_template', array('post' => $post[0]->data->children[0], 'user' => $user)); ?>
+	<? echo $this->load->view('templates/post_template', array('post' => $post[0]->data->children[0], 'user' => $user)); ?>
 </div>
