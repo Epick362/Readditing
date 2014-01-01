d8762e34923769471ca59d677a07038aa2423ba9
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-01-01 13:20:55 -0600

    Display

--- application/views/comments.php
+++ application/views/comments.php
@@ -1,1 +1,9 @@
-<pre><?print_r($post)?></pre>
+<?
+	if($post->kind == 't1') {
+		echo $this->load->view('templates/comment_template', array('comment' => $post));
+	}elseif($post->kind == 't3'){
+		echo $this->load->view('templates/post_template', array('post' => $post, 'user' => $user));
+	}else{
+		echo '...';
+	}
+?>
