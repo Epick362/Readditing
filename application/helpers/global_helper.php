a29c4d4f42c562b6e3b07201ac342e57167c2430
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-01-01 20:51:40 -0600

    Comments Fix

--- application/helpers/global_helper.php
+++ application/helpers/global_helper.php
@@ -7,5 +7,5 @@
 
 	function recursiveComments($post) {
 		$CI =& get_instance();
-		return $CI->load->view('templates/comments_template', array('comment' => $post), TRUE);
+		return $CI->load->view('templates/comments_template', array('comment' => $post, 'recursive' => TRUE), TRUE);
 	}
