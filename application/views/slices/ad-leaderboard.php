7f06e4a0ff179f228a5f642e8ef73c450e922000
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-07-11 10:12:26 +0200

    Ad fixes

--- application/views/slices/ad-leaderboard.php
+++ application/views/slices/ad-leaderboard.php
@@ -1,9 +1,11 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Leaderboard Main -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-6468416760530377"
-     data-ad-slot="4560424843"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<center style="margin-bottom: 20px">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- Leaderboard Main -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-6468416760530377"
+	     data-ad-slot="4560424843"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</center>
