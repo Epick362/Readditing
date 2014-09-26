dfbdfe9f4ff6f951fa061cb2d2bf19453fbc4db2
Epick362 <flp.hajek@gmail.com>
2014-09-26 15:25:10 +0200

    BSA

--- application/views/slices/ad-leaderboard.php
+++ application/views/slices/ad-leaderboard.php
@@ -1,12 +1,4 @@
-<!--
+
 <center style="margin-bottom: 20px">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<ins class="adsbygoogle"
-	     style="display:inline-block;width:728px;height:90px"
-	     data-ad-client="ca-pub-6468416760530377"
-	     data-ad-slot="4560424843"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<div id="bsap_1299172" class="bsarocks bsap_e5b2c2361c9aa558f3aae3449b24de26"></div>
 </center>
--->
